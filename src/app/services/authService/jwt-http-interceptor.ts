bab6e92e10956e5ed4baabfbc702c1c4a0b8fde6
Ade3k <adams.osseni@gmail.com>
2025-02-19 00:00:40 +0100

    Sans token

--- src/app/services/authService/jwt-http-interceptor.ts
+++ src/app/services/authService/jwt-http-interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const JwtHttpInterceptor: HttpInterceptorFn = (req, next) => {
   const clonedRequest = req.clone({
-    withCredentials: true, 
+    withCredentials: false, 
   });
   return next(clonedRequest);
 };
